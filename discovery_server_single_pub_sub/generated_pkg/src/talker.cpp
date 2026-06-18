2da8ab055a2549f6eea535c4714b5b40d32b2c17
sunny-nitrkl007 <aawara.sunny@gmail.com>
2026-06-19 01:04:08 +0530

    Version 1.0 - YAML driven ROS2 discovery server single pub/sub

---
+++ discovery_server_single_pub_sub/generated_pkg/src/talker.cpp
@@ -0,0 +1,42 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "talker.hpp"
+
+using namespace std::chrono_literals;
+
+Talker::Talker()
+    : Node("talker")
+{
+    this->declare_parameter<double>("publish_rate", 2.0);
+    publish_rate_ = this->get_parameter("publish_rate").as_double();
+
+    rclcpp::QoS qos(rclcpp::KeepLast(10));
+
+    
+    qos.reliable();
+    
+
+    publisher_ = this->create_publisher<std_msgs::msg::String>(
+        "chatter", qos);
+
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(static_cast<int>(1000.0 / publish_rate_)),
+        std::bind(&Talker::publish_message, this));
+}
+
+void Talker::publish_message()
+{
+    auto msg = std_msgs::msg::String();
+    msg.data = "Hello from talker! count=" + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg.data.c_str());
+    publisher_->publish(msg);
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<Talker>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ tool/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/ros/jazzy/setup.bash
+exec "$@"
